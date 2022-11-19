93d4f04a23b94e514acd67d25ffebe6f2e66fda1
theeb2k <tranbaohoi@gmail.com>
2022-11-19 14:48:21 +0700

    two commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("db.json"); // <== Will be created later
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3200; // <== You can change the port
 
-// Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
 server.use(router);
+
 server.listen(port);
